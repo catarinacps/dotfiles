6713b6923d7f7a79549dd964d859331928ad22aa
Henrique Silva <henrique.p.silva@outlook.com>
2019-05-01 15:22:37 -0300

    updated the clone script, now more readable

--- .scripts/clone_repo.sh
+++ .scripts/clone_repo.sh
@@ -3,16 +3,29 @@
 # This script is supposed to be downloaded and ran BEFORE the cloning of
 # this repository, i.e. run this script to clone
 
-alias dotgit='/usr/bin/git --git-dir=$HOME/.cfg. --work-tree=$HOME'
-echo "alias dotgit='/usr/bin/git --git-dir=$HOME/.cfg. --work-tree=$HOME'" >> $HOME/.bashrc
-echo ".cfg" >> $HOME/.gitignore
+# Definitions
+gitdir=".cfg/"
+gitrepo='git@github.com:hcpsilva/dotfiles.git'
+gitalias="git --git-dir=$HOME/$gitdir --work-tree=$HOME"
 
-git clone --bare https://github.com/hcpsilva/dotfiles.git $HOME/.cfg
+# Set-up
+alias dotgit="'$gitalias'"
+echo "alias dotgit='$gitalias'" >> $HOME/.$(basename $SHELL)rc
+echo $gitdir >> $HOME/.gitignore
 
+# Cloning
+git clone --bare $gitrepo $HOME/.cfg
+
+# Backup black magic
 mkdir -p .config-backup && dotgit checkout 2>&1 | \
     egrep "\s+"\. | awk {'print $1'} | \
     xargs -I{} mv {} .config-backup/{}
 
+# Make sure we checkout (if before where any duplicates)
 dotgit checkout
 
+# Set so that this repo can't see untracked files (otherwise madness)
 dotgit config --local status.showUntrackedFiles no
+
+# Load up the alias
+source $HOME/.$(basename $SHELL)rc
