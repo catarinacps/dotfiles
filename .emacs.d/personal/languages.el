b17abc279d098d4fd3826d49e697ba41b59e160a
Henrique Silva <henrique.p.silva@outlook.com>
2019-05-02 19:25:51 -0300

    renamed the install script

--- .emacs.d/personal/languages.el
+++ .emacs.d/personal/languages.el
@@ -2,6 +2,13 @@
 (require 'org)
 (require 'ess-r-mode)
 
+(defun insert-date-heading ()
+  (interactive)
+  (and (org-insert-heading-respect-content)
+       (insert (concat (format-time-string "%F") "\n"))))
+
+(setq-default org-display-custom-times t)
+
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
@@ -11,3 +18,6 @@
       (append '("-I" "include") irony-additional-clang-options))
 
 (setq org-highlight-latex-and-related '(latex script entities))
+(setq org-time-stamp-custom-formats '("<%F>" . "<%F %H:%M>"))
+
+(define-key org-mode-map (kbd "C-c x") 'insert-date-heading)
--- .scripts/install_repo.sh
+++ .scripts/install_repo.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
+
 # This script is supposed to be downloaded and ran BEFORE the cloning of
 # this repository, i.e. run this script to clone
 # Example usage: chmod +x ./clone_repo.sh; $SHELL ./clone_repo.sh
 
-
 # Definitions
-gitdir=".cfg/"
+gitdir="~/.cfg/"
 gitrepo='git@github.com:hcpsilva/dotfiles.git'
-gitalias="git --git-dir=$HOME/$gitdir --work-tree=$HOME"
+gitalias="git --git-dir=$gitdir --work-tree=$HOME"
 
 # Set-up
 alias dotgit="'$gitalias'"
@@ -14,7 +15,7 @@ echo "alias dotgit='$gitalias'" >> $HOME/.$(basename $SHELL)rc
 echo $gitdir >> $HOME/.gitignore
 
 # Cloning
-git clone --bare $gitrepo $HOME/.cfg
+git clone --bare $gitrepo $gitdir
 
 # Make sure we checkout (if before where any duplicates)
 dotgit checkout -f
