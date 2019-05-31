ef6d1f792ca41c5347969e459b61bfda82b7d165
Henrique Silva <henrique.p.silva@outlook.com>
2019-05-30 22:21:36 -0300

    woohoo multi monitor support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,5 +8,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+done
+
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+# polybar -c ~/.config/polybar/config.ini main &
