c109d868296aa887bb0bf20dda95a87ea3346f40
Henrique Silva <henrique.p.silva@outlook.com>
2020-02-04 13:21:14 -0300

    UID is a global variable

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,14 +2,14 @@
 
 ## Add this to your wm startup file.
 
-UID=$1
+USERID=$1
 HOME=$2
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $USERID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar -c $HOME/.config/polybar/config.ini main &
