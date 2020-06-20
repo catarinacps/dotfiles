d2f02c4f4dec11f5ddd4627bed55bc9885833021
Henrique Silva <hcpsilva@inf.ufrgs.br>
2020-06-20 01:11:21 -0300

    better watcher script

--- .scripts/sxhkd_watcher.sh
+++ .scripts/sxhkd_watcher.sh
@@ -3,43 +3,48 @@
 # a simple watcher script that tries to be smart about sxhkd chords
 
 # exits on failures
-set -eufx
+set -euf
 
-HOME=$1
+FIFO_PIPE="$1"
+
+# no pipe, no game
+[ -p "$FIFO_PIPE" ] || exit 1
 
 CONFIG_FILE="$HOME/.config/sxhkd/sxhkdrc"
-FIFO_PIPE="/tmp/sxhkd.fifo"
 
-NAMES_FILE="/tmp/watcher_names.tmp"
-KEYBINDINGS_FILE="/tmp/watcher_keybindings.tmp"
-PAIRS_FILE="/tmp/watcher_pairs.tmp"
+NAMES_FILE="$(mktemp)"
+KEYBINDINGS_FILE="$(mktemp)"
+PAIRS_FILE="$(mktemp)"
+
+# quit if sxhkd isn't running
+pgrep -x sxhkd > /dev/null || exit 1
 
 # collect all names from the annotations
-grep '#+chord:' "$CONFIG_FILE" | cut -d ' ' -f2- > $NAMES_FILE
+grep '#+chord:' "$CONFIG_FILE" | cut -d ' ' -f2- > "$NAMES_FILE"
 
 # collect the actual keys that follow said annotations
 sed -n '/#+chord:/{n; p;}' "$CONFIG_FILE" \
-    | tr -d ' ' | tr ';' ':' | cut -d ':' -f1 > $KEYBINDINGS_FILE
+    | tr -d ' ' | tr ';' ':' | cut -d ':' -f1 > "$KEYBINDINGS_FILE"
 
 # paste them together
-paste -d ':' $NAMES_FILE $KEYBINDINGS_FILE > $PAIRS_FILE
+paste -d ' ' "$NAMES_FILE" "$KEYBINDINGS_FILE" > "$PAIRS_FILE"
+
+# clean up the now useless temps
+rm -f "$NAMES_FILE" "$KEYBINDINGS_FILE"
 
 # while we read events
-cat $FIFO_PIPE | while read line; do
+cat "$FIFO_PIPE" | while read line; do
     # check what kind of event happened
-    case $line in
+    case "$line" in
         H*)
             # clean out the keypress
-            clean=$(echo $line | tr -d ' ' | cut -c2-)
+            clean=$(echo "$line" | tr -d ' ' | cut -c2-)
 
             # test all pairs always
-            while IFS=':' read -r name key; do
+            while read -r name key; do
                 # if we found our match, lets put it in the bar
-                if [ $clean = $key ]; then
-                    i3-msg mode "$name"
-                    break
-                fi
-            done < $PAIRS_FILE
+                [ "$clean" = "$key" ] && i3-msg mode "$name" && break
+            done < "$PAIRS_FILE"
             ;;
         EEnd*)
             # if we are ending a chain, back to the default mode
