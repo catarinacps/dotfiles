8d1d1d703bb45eddc6480c6940ff162f8bc918af
Henrique Silva <hcpsilva@inf.ufrgs.br>
2021-10-14 12:48:32 -0300

    fixed ranger python config indentation

--- .config/ranger/plugins/devicons_linemode.py
+++ .config/ranger/plugins/devicons_linemode.py
@@ -1,19 +1,21 @@
 import ranger.api
 from ranger.core.linemode import LinemodeBase
-from devicons import *
+from .devicons import *
+
 
 @ranger.api.register_linemode
 class DevIconsLinemode(LinemodeBase):
-  name = "devicons"
+    name = "devicons"
 
-  uses_metadata = False
+    uses_metadata = False
 
-  def filetitle(self, file, metadata):
-    return devicon(file) + ' ' + file.relative_path
+    def filetitle(self, file, metadata):
+        return devicon(file) + ' ' + file.relative_path
+
 
 @ranger.api.register_linemode
 class DevIconsLinemodeFile(LinemodeBase):
-  name = "filename"
+    name = "filename"
 
-  def filetitle(self, file, metadata):
-    return devicon(file) + ' ' + file.relative_path
+    def filetitle(self, file, metadata):
+        return devicon(file) + ' ' + file.relative_path
