c5d59ac8746136ac4936590b52c6a68a8370b886
Henrique Silva <hcpsilva@inf.ufrgs.br>
2021-10-14 12:48:59 -0300

    removed some modules from polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-USERID=$1
-HOME=$2
+USERID="$1"
+HOME="$2"
 
 # Terminate already running bar instances
-killall -q polybar
+pkill -u "$USERID" polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $USERID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$USERID" polybar >/dev/null; do sleep 1; done
 
-for m in "$(polybar --list-monitors | cut -d":" -f1)"; do
-    MONITOR="$m" polybar -c $HOME/.config/polybar/config.ini main &
+MON_LIST=$(xrandr --listmonitors | tail -n +2 | cut -d ' ' -f 3 | tr -d '+*')
+
+for m in $MON_LIST; do
+    MONITOR="$m" polybar -c "$HOME/.config/polybar/config.ini" main &
 done
 
 wait
