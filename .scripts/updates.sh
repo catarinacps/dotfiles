e40d7b7d0d887742f61d63348cd1e86331485ed2
Henrique Silva <hcpsilva@inf.ufrgs.br>
2022-03-06 14:23:15 -0300

    simplified the updates script

--- .scripts/updates.sh
+++ .scripts/updates.sh
@@ -1,32 +1,16 @@
 #!/bin/sh
 
-THEME="/usr/share/icons/$(gsettings get org.gnome.desktop.interface icon-theme | tr -d \')"
-
-ICON="$THEME/32x32/apps/system-software-update.svg"
-
-SHORT_SLEEP=300
+SHORT_SLEEP=600
 LONG_SLEEP=$((SHORT_SLEEP * 3))
 
 while true; do
-    if UPDATES=$(checkupdates 2>/dev/null); then
-        QTY=$(printf '%s\n' "$UPDATES" | wc -l)
+    UPDATES=$(checkupdates 2>/dev/null)
 
-        [ "$QTY" -gt 50 ] && URGENCY='critical' || URGENCY='normal'
+    QTY=$(printf '%s\n' "$UPDATES" | wc -l)
 
-        [ "$QTY" -gt 1 ] && PLURAL='s'
+    [ "$QTY" -gt 1 ] && PLURAL='S'
 
-        notify-send \
-            -u "$URGENCY" \
-            -i "$ICON" \
-            "Update$PLURAL available" \
-            "$QTY new package$PLURAL"
+    echo "$QTY UPDATE$PLURAL"
 
-        echo "$QTY update$PLURAL"  | tr '[:lower:]' '[:upper:]'
-
-        sleep $LONG_SLEEP
-    else
-        echo
-
-        sleep $SHORT_SLEEP
-    fi
+    sleep $LONG_SLEEP
 done
